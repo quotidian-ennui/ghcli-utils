165ac23227b3ef42c689c5c582bd84a0e2fceb72
Lewin Chan <quotidian-ennui@users.noreply.github.com>
2026-01-30 15:23:17 +0000

    feat: fake merge train (#1)
    - add a fake merge-train
    - add copilot instructions because annoying
    - add ghcli-common for shared functions
    - add a precommit for hooking

---
+++ ghcli-common.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+GH_REST_API_VERSION="X-GitHub-Api-Version: 2022-11-28"
+GH_ACCEPT="Accept: application/vnd.github+json"
+
+gh_api() {
+  gh api -H "$GH_REST_API_VERSION" -H "$GH_ACCEPT" "$@"
+}
+
+compressQuery() {
+  printf '%s' "$1" | tr -d '\n'
+}
+
+# Because I often just use GH_TOKEN, and don't have 'gh config get user' available.
+# This is _weak_ but it will work for my purposes.
+gh_whoami() {
+  local me=""
+  me=$(gh config get user -h github.com 2>/dev/null || true)
+  if [[ -z "$me" ]]; then
+    me=$(gh auth status -h github.com | grep "Logged in" | sed -e "s/^[[:blank:]]*//" -e "s/[[:blank:]]*$//" | cut -f7 -d ' ')
+  fi
+  echo "$me"
+}
